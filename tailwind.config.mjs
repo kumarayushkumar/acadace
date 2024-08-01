94cb69d2812de1077ebb9f735730081c4c825924
Ayush kumar <kumarayushkumar@protonmail.com>
2024-08-01 20:29:55 +0530

    chore: Remove unused favicon.svg and update index page layout to use custom font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: ['Helvetica', ...defaultTheme.fontFamily.sans],
+      main: ['Helvetica-rounded', ...defaultTheme.fontFamily.sans],
+      hightlight: ['Georgia', ...defaultTheme.fontFamily.serif],
+    },
   },
   plugins: []
 }
