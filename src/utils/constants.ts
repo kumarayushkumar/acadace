b064cbeb92ab0aa364c12bd16258fdfa3976d5b1
Ayush kumar <kumarayushkumar@protonmail.com>
2024-08-11 17:46:18 +0530

    chore: created footer section, fixed links in header and minor improvements

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,6 @@
 export const CONST = {
   slogan: 'Ace the Acadamics',
-  phone: '9479325238'
+  phone: '9479325238',
+  description:
+    'AcadAce offers comprehensive academic support for college students, including minor and major projects, term papers, and certification courses. We deliver high-quality, well-researched work from start to finish, ensuring your academic success. Contact us today to excel in your studies!'
 }
